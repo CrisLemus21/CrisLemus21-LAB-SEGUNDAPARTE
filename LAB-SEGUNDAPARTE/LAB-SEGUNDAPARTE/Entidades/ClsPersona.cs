d5b57a0ac67ab2322f7f7ddd348805d752097477
MigueGQ <gq0392032021@unab.edu.sv>
2021-08-30 15:28:28 -0600

    v2

--- LAB-SEGUNDAPARTE/LAB-SEGUNDAPARTE/Entidades/ClsPersona.cs
+++ LAB-SEGUNDAPARTE/LAB-SEGUNDAPARTE/Entidades/ClsPersona.cs
@@ -8,5 +8,43 @@ namespace LAB_SEGUNDAPARTE.Entidades
 {
     class ClsPersona
     {
+        private string nombre;
+        private int edad;
+        private string sexo;
+        private double peso;
+        private double altura;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public double Peso { get => peso; set => peso = value; }
+        public double Altura { get => altura; set => altura = value; }
+
+        public ClsPersona(string nombre, int edad, string sexo)
+        {
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Sexo = sexo;
+        }
+
+        public ClsPersona(string nombre, int edad, string sexo, double peso, double altura)
+        {
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Sexo = sexo;
+            this.Peso = peso;
+            this.altura = altura;
+        }
+
+        public ClsPersona(double peso, int altura)
+        {
+            this.Peso = peso;
+            this.altura = altura;
+        }
+
+        public override string ToString()
+        {
+            return nombre + " " + edad + " " + sexo + " " + peso + " " + altura;
+        }
     }
 }
