e7cf733b18094e73449c894b88aee71e77527ddf
CrisLemus21 <87676318+CrisLemus21@users.noreply.github.com>
2021-08-30 15:57:40 -0600

    actualizacio v2

--- LAB-SEGUNDAPARTE/LAB-SEGUNDAPARTE/Negocio/NClsPersona.cs
+++ LAB-SEGUNDAPARTE/LAB-SEGUNDAPARTE/Negocio/NClsPersona.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LAB_SEGUNDAPARTE.Entidades;
 
 namespace LAB_SEGUNDAPARTE.Negocio
 {
     class NClsPersona
+
     {
+        ClsPersona per = new ClsPersona(0,0);
+        public string calcularIMC()
+        {
+            
+
+            double pro = per.Peso / Math.Pow(per.Altura, 2);
+
+
+            if (pro < 20)
+            {
+                return "peso es ideal";
+            }
+            if (pro > 25)
+            {
+                return "tiene sobre peso";
+            }
+            return "";   
+            
+            
+        }   
+        public string esMayorDeEdad()
+        {
+            if (per.Edad >= 18)
+            {
+                return "Es mayor de edad";
+            }
+            else
+                return "No es mayor de edad";
+           
+        }
     }
 }
