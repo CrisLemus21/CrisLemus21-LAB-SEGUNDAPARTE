ed134e4594c31d0501c0f9dad093e6944fd10cca
JordyC21 <87671558+JordyC21@users.noreply.github.com>
2021-08-30 16:31:07 -0600

    Cambios(Jordy)

--- LAB-SEGUNDAPARTE/LAB-SEGUNDAPARTE/Program.cs
+++ LAB-SEGUNDAPARTE/LAB-SEGUNDAPARTE/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LAB_SEGUNDAPARTE.Entidades;
+using LAB_SEGUNDAPARTE.Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,29 @@ namespace LAB_SEGUNDAPARTE
     {
         static void Main(string[] args)
         {
+            ClsPersona clsPersona = new ClsPersona();
+            NClsPersona nclspersona = new NClsPersona();
+
+            Console.WriteLine("Escriba los siguientes datos" +
+            "\n\n Escribe tu nombre :");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Ahora edad: ");
+            int edad = Convert.ToInt32 ( Console.ReadLine());
+            Console.WriteLine("Ingrese el sexo  : ");
+            string sexo = Console.ReadLine();
+            Console.WriteLine("Ingresa la altura : ");
+            string altura = Console.ReadLine();
+
+
+            ClsPersona clsPersona1 = new ClsPersona(nombre, edad, sexo); 
+
+
+
+
+
+
+
+
         }
     }
 }
